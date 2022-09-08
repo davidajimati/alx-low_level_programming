5c1853c6b088cb6468c8f8a2057884eb840ec9bc
davidajimati <ajimatidavid@gmail.com>
2022-09-08 09:17:07 -0700

    Size of Data types Scripts

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Starting point / entry
+ *
+ * Return:0 when succesful
+ */
+int main(void)
+{
+	/* Here I declare my variables */
+	char char_type;
+	int int_type;
+	long int long_int_type;
+	long long int long_long_int_type;
+	float float_type;
+
+	/* Here's where we declare the work. */
+	printf("size of char: %d byte(s)\n", sizeof char_type);
+	printf("Size of an int: %d byte(s)\n", sizeof int_type);
+	printf("Size of a long int: %d byte(s)\n", sizeof long_int_type);
+	printf("Size of a long long int: %d byte(s)\n", sizeof long_long_int_type);
+	printf("Size of a float: %d byte(s)\n", sizeof float_type);
+
+	return (0);
+}
