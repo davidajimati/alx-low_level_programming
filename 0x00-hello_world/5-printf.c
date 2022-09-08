0339c51976ee76ad1c4dcb50f86dcad74dc79ebb
davidajimati <ajimatidavid@gmail.com>
2022-09-08 08:30:34 -0700

    Hello PrintF

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Program entry point
+ *
+ * Return: Shows success status when it returns 0
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
