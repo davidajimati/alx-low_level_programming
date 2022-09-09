35ae7a31689819b4b9c5d0b58f32c46d16516488
davidajimati <ajimatidavid@gmail.com>
2022-09-09 16:10:57 -0700

    Script 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+	char out;
+
+	for (num = 0 ; num <= 9 ; num++)
+	{
+		out = number + '0';
+		putchar(out);
+
+		if (out != '9')
+			putchar(' ');
+			putchar(',');
+	}
+	putchar('\n');
+	return (0);
+}
