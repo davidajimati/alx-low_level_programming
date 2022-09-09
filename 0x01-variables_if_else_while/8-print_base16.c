e7e5528a475c9e569ad9d2bb876ca826da8add38
davidajimati <ajimatidavid@gmail.com>
2022-09-09 15:55:22 -0700

    Print Base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+	char figures;
+	char alpha;
+
+	for (num = 0 ; num < 10 ; num++)
+	{
+		figures = (num + '0');
+		putchar(figures);
+	}
+	for (alpha = 'a' ; alpha <= f ; alpha++);
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
+}
