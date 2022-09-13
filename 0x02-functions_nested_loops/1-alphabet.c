52f3b977071c9b47bec5317e83f82f16d8916d12
davidajimati <ajimatidavid@gmail.com>
2022-09-13 21:46:00 +0000

    FInal edit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,7 +5,7 @@
  *
  * Return: Always 0 (Success)
  */
-int alphabet(void)
+void print_alphabet(void)
 {
 	char i;
 
@@ -14,5 +14,4 @@ int alphabet(void)
 		_putchar(i);
 	}
 	_putchar('\n');
-	return (0);
 }
