360c6b749c942cb1588819cc4441aa802f008ac0
davidajimati <ajimatidavid@gmail.com>
2022-09-13 23:59:21 +0000

    EDit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - evaluates the absolute value of an integer
+ *
+ * @int - integer
+ * Return: returns 0 when successful
+ */
+int _abs(int)
+{
+	int abs(int);
+	return (0);
+}
