8b76d5dfe5c9c038474f290f46a1cb310ea78251
davidajimati <ajimatidavid@gmail.com>
2022-09-13 21:59:23 +0000

    X10 alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints a letter 10 times
+ *
+ * Return: Letf out into the main function
+ */
+void print_alphabet_x10(void)
+{
+	int i = 1;
+	char start;
+
+	while (i <= 10)
+	{
+		for (start = 'a' ; start <= 'z': start++)
+		{
+			_putchar(start);
+		}
+		_putchar('\n');
+	}
+	i++;
+}
