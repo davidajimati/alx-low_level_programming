55478a616ea73cd3386f842a4165594283c233d3
davidajimati <ajimatidavid@gmail.com>
2022-09-13 19:21:06 +0000

    Script Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Main entry to program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	return (0);
+}
