3a1649250c2399b9dd12908771c4e57048ca4241
davidajimati <ajimatidavid@gmail.com>
2022-09-14 15:34:30 +0000

    Script added

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <sdlib.h>
+
+/**
+ * print_to_98 - Mian function; prints from any number to 98
+ *
+ * @n: the integer passed into function
+ * Return: Always 0 (Success)
+ */
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+	if else(n == 98)
+		printf("%d", n);
+	else
+	{
+		while (n <= 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+	}
+	}
+}
