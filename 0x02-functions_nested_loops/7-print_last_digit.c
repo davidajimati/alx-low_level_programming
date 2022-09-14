564391c8af707e7eb1926ad240ac5f2833aae7b4
davidajimati <ajimatidavid@gmail.com>
2022-09-14 00:30:18 +0000

    print last digit script

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main"
+#include <stdio.h>
+
+/**
+ * print_last_digit - function to perform the action
+ *
+ * @i: main integer
+ * Return: returns the last number of i
+ */
+int print_last_digit(int i)
+{
+	int last;
+
+	last = i % 10;
+	return (last);
+}
