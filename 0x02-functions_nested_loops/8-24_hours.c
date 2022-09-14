d4bf717e2672ff34fe7aeaa9fb7e3a3d73b7f813
davidajimati <ajimatidavid@gmail.com>
2022-09-14 10:03:57 +0000

    Jack beauer edit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -21,7 +21,7 @@ void jack_bauer(void)
 			_putchar(hours / 10 + '0');
 			_putchar(hours_remain + '0');
 			_putchar(':');
-			_putchar(mins / 10 + '0');
+			_putchar(minutes / 10 + '0');
 			_putchar(mins_remain + '0');
 			_putchar('\n');
 			minutes++;
