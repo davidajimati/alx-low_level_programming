8cb52cc71881aa8b4d088418debac0ac490e8f80
davidajimati <ajimatidavid@gmail.com>
2022-09-14 15:13:51 +0000

    Script added

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers
+ *
+ * @int: denotes the integer to be added
+ * Return: Returns the result
+ */
+int add(int, int)
+{
+	int a, b, sum;
+
+	sum = a + b;
+	return (sum);
+}
