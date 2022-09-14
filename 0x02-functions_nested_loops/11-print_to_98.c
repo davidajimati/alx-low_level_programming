10e5e1416adc8a80ef20e19981feb3dfc2466f5b
davidajimati <ajimatidavid@gmail.com>
2022-09-14 21:28:52 +0000

    Script added

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -19,12 +19,12 @@ void print_to_98(int n)
 		while (n >= 98)
 		{
 			printf("%d", n);
+			n--;
 			if (n > 98)
 			{
 				_putchar(',');
 				_putchar(' ');
 			}
-			n--;
 		}
 		_putchar('\n');
 	}
@@ -33,12 +33,12 @@ void print_to_98(int n)
 		while (n <= 98)
 		{
 			printf("%d", n);
+			n++;
 			if (n < 98)
 			{
 				_putchar(',');
 				_putchar(' ');
 			}
-			n++;
 		}
 		_putchar('\n');
 	}
