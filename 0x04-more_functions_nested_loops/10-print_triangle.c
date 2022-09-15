71e6c3763f4f50b02f5cd80ddde42a4ec9000195
davidajimati <ajimatidavid@gmail.com>
2022-09-15 22:17:54 +0000

    Script added

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -15,13 +14,15 @@ void print_triangle(int size)
 	{
 		for (y = 0; y < size; y++)
 		{
-			for (x = (size - y); x > 1; x--)
+			for (x = size - y; x > 1; x--)
 				_putchar(32);
-			for (z = 0; x <= y; x++)
+			for (z = 0; z <= y; z++)
 				_putchar(35);
 		}
 	_putchar('\n');
 	}
 	else
+	{
 		_putchar('\n');
+	}
 }
