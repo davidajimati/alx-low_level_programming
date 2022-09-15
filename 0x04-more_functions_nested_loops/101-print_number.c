17db082d15d7db4b53a703c55bfdacdcd69d5e52
davidajimati <ajimatidavid@gmail.com>
2022-09-15 22:29:41 +0000

    Script added

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_number - main function
+ *
+ * @n: variable in question
+ * Return: void in this function
+ */
+void print_number(int n)
+{
+	if (n > 9)
+	{
+		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
+		_putchar('\n');
+	}
+	else
+		_putchar(n);
+	_putchar('\n');
+}
