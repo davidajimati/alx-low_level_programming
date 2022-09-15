5e048a4ef0afecd9eaeaf6644b8f97cc97abcf33
davidajimati <ajimatidavid@gmail.com>
2022-09-15 16:58:43 +0000

    Script added

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagonal - main function
+ *
+ * @n: determinant variable
+ * Return: Void in this function
+ */
+void print_diagonal(int n)
+{
+	int i;
+	char chr = ' ';
+	char slash = '\';
+
+	for (i = 1; i <= n; i++)
+	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+		else
+		{
+			while (i > 1)
+			{
+				_putchar(chr);
+				i--;
+			}
+			_putchar(slash);
+		}
+	}
+	_putchar('\n');
+}
