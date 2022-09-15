8afc77dca99869a3ac380b55945c7db762a85f1a
davidajimati <ajimatidavid@gmail.com>
2022-09-15 17:40:26 +0000

    Script added

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -17,11 +17,11 @@ void print_diagonal(int n)
 	}
 	else
 	{
-		for (i = 1; i <= n; i++)
+		for (i = 0; i < n; i++)
 		{
 			for (x = 0; x < i; x++)
 			{
-				_putchar(' ');
+				_putchar(32);
 			}
 			_putchar(92);
 			_putchar('\n');
