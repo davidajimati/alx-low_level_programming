2293d08c9d10a8f95e572ceec7f570a6c76a63f5
davidajimati <ajimatidavid@gmail.com>
2022-09-15 20:41:51 +0000

    Script added

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,16 +5,15 @@
 /**
  * main - Fizz-Buzz function
  *
- * @i: determinant variable
  * Return: Always 0
  */
 
-int main(int i)
+int main(void)
 {
 	int i, a;
 	char space = ' ';
 
-	for (i = 1; i < 100; 1++)
+	for (i = 1; i <= 100; i++)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
 			printf("FizzBuzz");
