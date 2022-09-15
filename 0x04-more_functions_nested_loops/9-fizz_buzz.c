ceccaaba1d60f6144a6f8bea782598cadb3f1d55
davidajimati <ajimatidavid@gmail.com>
2022-09-15 20:40:27 +0000

    Script added

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,24 +17,16 @@ int main(int i)
 	for (i = 1; i < 100; 1++)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
-		{
 			printf("FizzBuzz");
-		}
 		else if ((i % 3) == 0)
-		{
 			printf("Fizz");
-		}
 		else if ((i % 5) == 0)
-		{
 			printf("Buzz");
-		}
 		else
 			printf("%d", i);
 		for (a = 0; a < 99; a++)
-		{
 			printf("%c", space);
-		}
 	}
-	putchar('\n');
+	printf("%c", space);
 	return (0);
 }
