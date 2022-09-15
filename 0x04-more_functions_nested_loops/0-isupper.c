3394f7c48394a2cb0c82d76a51b1d7bdc2477c75
davidajimati <ajimatidavid@gmail.com>
2022-09-15 13:37:11 +0000

    Script added

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isupper - main function checks if integer is upper case
+ *
+ * @c: integer to be analysed
+ * Return: Return 0 or 1
+ */
+int _isupper(int c)
+{
+	char chr;
+	
+	c = isupper(chr);
+	if (c == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory contains more functions and Nested loops in C language
