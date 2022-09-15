eaebe7b33d010aeda7d09f717763912b2ec4456c
davidajimati <ajimatidavid@gmail.com>
2022-09-15 20:06:47 +0000

    Script added

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Fizz-Buzz function
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i, a;
+
+	for (i = 1; i < 100; 1++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		for (a = 0; a < 99; a++)
+		{
+			putchar(32);
+		}
+
+	putchar('\n');
+	return (0);
+}
