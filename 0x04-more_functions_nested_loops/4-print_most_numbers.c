dd3872a0541aefd16ce2b1f2ecf0365e8e612764
davidajimati <ajimatidavid@gmail.com>
2022-09-15 14:54:51 +0000

    Script added

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_most_numbers - main function
+ *
+ * Return: Void in this function
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		ans = i + '0';
+		if (i != 2 || i != 4)
+			_putchar(ans);
+		_putchar('\n');
+	}
+}
