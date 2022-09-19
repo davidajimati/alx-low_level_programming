d14e3d3bedbf9d6b2172f1c3779d1af51d0c2a90
davidajimati <ajimatidavid@gmail.com>
2022-09-19 19:09:16 +0000

    Script added

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * rev_string - reverses string
+ *
+ * @s: string variable
+ * Return: ALways 0
+ */
+void rev_string(char *s)
+{
+	int leng;
+
+	while (s[leng] != '\0')
+	{
+		leng++
+	}
+	for (leng -= 1; leng >= 0; leng--)
+	{
+		_putchar(s[leng]);
+	}
+	_putchar('\n');
+}
