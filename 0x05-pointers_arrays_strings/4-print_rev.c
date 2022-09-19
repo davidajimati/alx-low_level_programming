d4201031165142cbf797c689b2822a43533afe54
davidajimati <ajimatidavid@gmail.com>
2022-09-19 18:22:58 +0000

    Script added

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_rev - prints reverse of a string
+ *
+ * @s: string variable
+ * Return: defined in main function
+ */
+
+void print_rev(char *s)
+{
+	char i;
+
+	i = '\0';
+	while (i > *s)
+	{
+		_putchar(s);
+		i--;
+	}
+	_putchar('\n')l
+}
