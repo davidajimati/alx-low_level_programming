3a5113a72ef545a53e4662dff704a807cbc1e252
davidajimati <ajimatidavid@gmail.com>
2022-09-19 13:57:47 +0000

    Script added

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * swap_int - swaps the value of 2 integers
+ *
+ * @b: the subject variable
+ * @a: Variable 2
+ * Return: void function, declared in main function
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
