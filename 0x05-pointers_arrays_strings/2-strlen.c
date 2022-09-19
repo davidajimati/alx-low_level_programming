5ab43eb348b3e90800e8cee20f20f322d83ee495
davidajimati <ajimatidavid@gmail.com>
2022-09-19 16:17:23 +0000

    Script added

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen - prints the length of a string
+ *
+ * @s: string to be considered
+ * Return: Defined in the main function
+ */
+
+int _strlen(char *s)
+{
+	int len = strlen(*s);
+}
