180df2528e4eb443d0c4187c1a2a593a468ab085
davidajimati <ajimatidavid@gmail.com>
2022-09-19 16:39:30 +0000

    Script added

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * _puts - prints a string followed by a new line
+ *
+ * @str: The string to be printed
+ * Return: Always 0 - defined in main
+ */
+void _puts(char *str)
+{
+	puts(*str);
+}
