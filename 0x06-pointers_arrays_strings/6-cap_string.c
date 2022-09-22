3370c71dfe22ac09c87e19401f231ebb067b6ff6
davidajimati <ajimatidavid@gmail.com>
2022-09-22 23:20:57 +0000

    Script added

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,25 +2,29 @@
 #include <string.h>
 
 /**
- * *cap_string - capitalizes all words of a string
+ * *cap_string - Chages lowercase to uppercase
  *
- * @str: string to be capitalized
- * Return: char type
+ * @s: string variable
+ * Return: variable c
  */
 
-char *cap_string(char str[])
+char *cap_string(char *s)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (str[i] == ',' || str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
+		if (s[i] >= 97 && s[i] <= 122)
 		{
-			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+			if (s[i] == 44 || s[i] == 59 || s[i] == 46 || s[i] == 33)
 			{
-				str[i + 1] -= 32;
+				s[i + 1] -= 32;
 			}
 		}
+		else if (s[i] == 63 || s[i] == 34 || s[i] == 40 || s[i] == 41 || s[i] == 123 || s[i] == 125)
+		{
+			s[i + 1] -= 32;
+		}
 	}
-	return (str);
+	return (s);
 }
