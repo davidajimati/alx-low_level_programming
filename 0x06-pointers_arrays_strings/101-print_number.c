e327b852cea27b54a252fa95a9ad8fad7014fd37
davidajimati <ajimatidavid@gmail.com>
2022-09-23 16:23:55 +0000

    Script added

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,15 +6,31 @@
  * @n: integer to be printed
  * Return: nothing -  void function
  */
-
 void print_number(int n)
 {
-	if (n < 0)
-		_putchar('-');
+	unsigned int tens, number, pos = n;
+	double t = 1;
 
-	if (n > 9)
+	if (n == 0)
+		_putchar('0');
+	else
 	{
-		_putchar(n / 10);
-		_putchar((n % 10) + '0');
+		if (n < 0)
+		{
+			pos = n * -1;
+			_putchar('-');
+		}
+
+		while (t <= pos)
+			t *= 10;
+		tens = t / 10;
+
+		while (tens >= 1)
+		{
+			number = pos / tens;
+			_putchar(number + '0');
+			pos = (pos - (tens * number));
+			tens /= 10;
+		}
 	}
 }
