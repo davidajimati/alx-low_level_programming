600e8385a925ee9b209032980d4d0f004e91b870
davidajimati <ajimatidavid@gmail.com>
2022-09-26 22:26:51 +0000

    Script added

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,18 +7,19 @@
  * @c: character to be searched for
  * Return: Pointer to the first occurence of c
  */
-
 char *_strchr(char *s, char c)
 {
-	if (*s != c)
-	{
-		s++;
+	while (*s)
+	{}
+		if (*s != c)
+			s++;
+		else
+			return (s);
 	}
-	else
-		return (s);
 
 	if (c == '\0')
 		return (s);
 
-	return ('\0');
+	return (NULL);
 }
+
