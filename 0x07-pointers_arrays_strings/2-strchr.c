9441449e7fa38e0a6af46419c4b4159a8611279f
davidajimati <ajimatidavid@gmail.com>
2022-09-26 21:26:30 +0000

    Script added

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *_strchr - Locates a character in a string and allocates a pointer
+ *
+ * @s: string to be examined
+ * @c: character to be searched for
+ * Return: Pointer to the first occurence of c
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+	char *point;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+		point = &(s[i]);
+			return (point);
+		}
+	}
+	return ('\0');
+}
