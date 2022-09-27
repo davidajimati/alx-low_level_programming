cf6b3c2ce62a37ea2fa42b12f439e2b6c6ba663a
davidajimati <ajimatidavid@gmail.com>
2022-09-27 21:30:28 +0100

    strstr function created

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* *_strstr - locates a substring
+*
+* @haystack: string
+* @needle: substring to be searched
+* Return: points to the beginning of needle in haystack or NULL
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j = 0, occur, det;
+
+	if (needle[j] == '\0')
+		return (NULL);
+	if (haystack[j] == '\0')
+		return (NULL);
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i] == needle[j])
+				occur = i;
+			det = occur;
+		}
+
+		while (haystack[i] == needle[j] && haystack[i] != '\0' && needle[j] != '\0')
+		{
+			det++;
+		}
+		if (haystack[det] != '\0')
+		{
+			return (haystack + occur);
+		}
+	}
+	return ('\0');
+}
