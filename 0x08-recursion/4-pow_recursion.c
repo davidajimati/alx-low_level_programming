8e63c60f29d6421efc15d8f87e09cdc12776a1c3
davidajimati <ajimatidavid@gmail.com>
2022-09-28 23:53:07 +0100

    EDit

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* _pow_recursion - returns the value of x raised to y
+*
+* @x: integer 1
+* @y: integer 2
+* Return: power of x to y
+*/
+
+int _pow_recursion(int x, int y)
+{
+	return (x**y);
+}
