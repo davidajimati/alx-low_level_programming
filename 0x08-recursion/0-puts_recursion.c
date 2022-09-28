61d30ca5d4cd344d720ac9733fa5628dfca7fd72
davidajimati <ajimatidavid@gmail.com>
2022-09-28 15:58:51 +0100

    Puts fucntion created

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _puts_recursion - prints a string to stdout followed by a new line
+*
+* @s: string
+* Return: Nothing
+*/
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+
+	_putchar(*s);
+	_puts_recursion(s + 1);
+	
+	if (*(s + 1) == '\0')
+	_putchar('\n');
+}
