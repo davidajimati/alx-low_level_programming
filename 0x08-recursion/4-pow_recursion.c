3d88b1bbf136a28d2784b69a058bb632bcc0bdd6
davidajimati <ajimatidavid@gmail.com>
2022-09-29 00:30:29 +0100

    pow function added

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -16,8 +16,6 @@ int _pow_recursion(int x, int y)
 			return (1);
 		return (x * _pow_recursion(x, (y - 1)));
 	}
-	else if (y == -1)
-		return (-1);
 	else
-		return (1 / (x * _pow_recursion(x, ((-1 * y) - 1))));
+		return (-1);
 }
