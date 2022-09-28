44930476f1d40c431f10430a75c72c69873d8c2b
davidajimati <ajimatidavid@gmail.com>
2022-09-28 21:59:19 +0100

    Puts reverse function

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,42 +1,21 @@
 #include "main.h"
-int counter(char *s, int j);
 
 /**
 * _print_rev_recursion - prints a string in reverse
 *
 * @s: string
-* Return: Nothing
+* Return: nothing
 */
 
 void _print_rev_recursion(char *s)
 {
-	int j;
-
-	if (j == -1)
-		return;
-
-	j = counter(s, j);
-	_putchar(*(s + (j - 1)));
-	_print_rev_recursion(s);
-}
-
-/**
-* counter - counts the number of characters in  a string
-*
-* @string: string to be counted
-* @j: integer to be returned
-* Return: int j
-*/
-
-int counter(char *s, int j)
-{
-	int i = 0;
-
-	j = 0;
-	if (*(s +  i) != '\0')
+	if (s == '\0')
 	{
-		i++;
+		;
 	}
-	j = i;
-	return (j);
+	else
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
