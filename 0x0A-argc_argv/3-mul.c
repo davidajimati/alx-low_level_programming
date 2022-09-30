d56cfd3ae3f3f11df4d6ae33d2e93dc246703723
davidajimati <ajimatidavid@gmail.com>
2022-09-30 23:38:20 +0100

    Program that multiplies integers in CLI

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Funtion that multiplies 2 numbers
+* @argc: argument count
+* @argv: argument vector
+* Return: Always 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int mul = 0;
+
+	if (argc < 3 || argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	mul = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", mul);
+	return (0);
+}
