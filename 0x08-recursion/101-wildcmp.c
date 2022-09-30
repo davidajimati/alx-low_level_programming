e2eb8a58fde49c381e976e554270d1502cdda182
davidajimati <ajimatidavid@gmail.com>
2022-09-30 02:04:36 +0100

    Script edit

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -13,7 +13,7 @@ int wildcmp(char *s1, char *s2)
 	if ((*s1 != *s2) && (*s2 != '*'))
 		return (0);
 
-	if ((*s1 == *s2) && (*s2 == '*'))
+	if (*s1 == *s2)
 		return (1);
 
 	return (wildcmp(s1, (s2 + 1)));
