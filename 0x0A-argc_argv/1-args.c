732e2ba46c6cd6166e92bd86514bc4abee95781b
davidajimati <ajimatidavid@gmail.com>
2022-09-30 19:59:04 +0100

    Script that prints number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - prints the number of command line arguments
+* @argc: argument count
+* Return: Always 0
+*/
+
+int main(int argc)
+{
+	printf("%d\n", argc);
+	return (0);
+}
