b0a93b048f7777072f7dea4ee817cf1ea606de6a
davidajimati <ajimatidavid@gmail.com>
2022-09-30 01:56:34 +0100

    wildcmp sile added

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,1 +1,20 @@
 #include "main.h"
+
+/**
+* wildcmp -  compares two strings and returns 1 if identical or 0 if not
+*
+* @s1: main string
+* @s2: wildcmp string
+* Return: 1 if succesful Or 0 if not
+*/
+
+int wildcmp(char *s1, char *s2)
+{
+	if ((*s1 != *s2) && (*s2 != '*'))
+		return (0);
+
+	if ((*s1 == *s2) && (*s2 == '*'))
+		return (1);
+
+	return (wildcmp(s1, (s2 + 1)));
+}
