b52cf35832578a1df05b58bf1d5792cfe86a4385
davidajimati <ajimatidavid@gmail.com>
2022-09-30 19:55:16 +0100

    FIle to print it's own name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,15 @@
-#!/bin/bash
-gcc -c *.c
+#include <stdio.h>
 
-ar rc liball.a *o
+/**
+* main - funtion to print out the name of the file
+* @argc: argument count
+* @argv: argument vector
+* Return: Always 0
+*/
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", *(argv + 0));
+	return (0);
+}
