409061217125d9f57a9c30438e1072954b5caf5c
davidajimati <ajimatidavid@gmail.com>
2022-10-01 01:39:12 +0100

    edit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,8 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+int check(char *s);
 
 /**
 * main - adds positive numbers
@@ -10,23 +13,59 @@
 
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int i, j, detect = 0, sum = 0;
 
 	if (argc < 2)
 	{
 		printf("0\n");
 		return (0);
 	}
-
+	
 	for (i = 0; i < argc; i++)
 	{
-		if (atoi(*(argv[i])) < 0 || atoi(*(argv[i])) > 9)
-		{
-			printf("Error\n");
-			return (1);
-		}
-		sum += atoi(argv[i]);
+		detect = check(argv[i]);
+
+		if (detect == 1)
+			break;
 	}
-	printf("%d\n", sum);
+
+	if (detect == 1)
+	{
+		printf("Error\n");
+		return(1);
+	}
+	
+	else
+	{
+		for (j = 1; j < argc; j++)
+		{
+			sum += atoi(argv[i]);
+		}
+		printf("%d\n", sum);
+	}
 	return (0);
+}
+
+/**
+* check - checks if a variable a digit
+* @num: variable
+* Return: 0 when succesful, 1 when false
+*/
+
+int check(char *s)
+{
+	int a;
+	int check = 0;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		if (!isdigit(s[a]))
+		{
+			check = 1;
+			break;
+		}
+		else
+			check = 0;
+	}
+	return (check);
 }
