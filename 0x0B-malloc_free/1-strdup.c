5e01da0face63510320dd6999b8fac38d74dcee0
davidajimati <ajimatidavid@gmail.com>
2022-10-03 19:31:12 +0100

    STrdup funtion created

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+* _strdup -  returns pointer to the newly allocated space in memory
+*
+* @str: string to be copied
+* Return: returns a char pointer
+*/
+
+char *_strdup(char *str)
+{
+	int i;
+	char *p;
+
+	p = malloc(strlen(str));
+
+	if (str != NULL)
+	{
+		for (i = 0; str[i] != '\0'; i++)
+		{
+			p[i] = str[i];
+		}
+		return (p);
+	}
+	else
+		return (NULL);
+	free(p);
+}
