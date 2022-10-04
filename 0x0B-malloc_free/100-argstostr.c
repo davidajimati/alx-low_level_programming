3b11c94f0ec09a05dedb1f2556a0c56857e4c4cd
davidajimati <ajimatidavid@gmail.com>
2022-10-04 10:40:54 +0100

    Edit

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -20,7 +20,7 @@ char *argstostr(int ac, char **av)
 		return (NULL);
 	}
 
-	str = malloc(sizeof(av) * ac);
+	str = malloc(sizeof(**av) * ac);
 
 	if (str == NULL)
 		return (NULL);
@@ -33,4 +33,5 @@ char *argstostr(int ac, char **av)
 		}
 	}
 	return (str);
+	free(str);
 }
