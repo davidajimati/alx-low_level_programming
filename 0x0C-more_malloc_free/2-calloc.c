f06a12e60d0ed5a80e340aff0cc9955b176ca74e
davidajimati <ajimatidavid@gmail.com>
2022-10-08 21:20:19 +0100

    CAlloc function created

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -6,21 +6,22 @@
 *
 * @nmemb: number of elements in array
 * @size: bytes of each element
-* Return: Void
+* Return: Returns pointer to allocated memory or NULL if fail
 */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int *array;
+	char *array;
 	unsigned int i, total;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
 
-	total = (size * nmemb) + 1;
+	total = size * nmemb;
 
-	array = malloc((size * nmemb) + 1);
+	array = malloc(size * nmemb);
+
 	if (array == NULL)
 		return (NULL);
 
