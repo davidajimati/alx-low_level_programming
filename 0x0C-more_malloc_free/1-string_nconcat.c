bf54acbb99a25a88be4cc145b6b4d0523747771f
davidajimati <ajimatidavid@gmail.com>
2022-10-08 15:02:17 +0100

    memory allocation edited

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -23,10 +23,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (n == strlen(s2) || n > strlen(s2))
 	{
-		ptr = malloc(sizeof(s1) + strlen(s2));
+		ptr = malloc(sizeof(char) * (strlen(s1) + strlen(s2) + 1));
 	}
 	else
-		ptr = malloc(sizeof(s1) + n);
+		ptr = malloc(sizeof(char) * (strlen(s1) +  n + 1));
 
 	if (ptr == NULL)
 		return (NULL);
