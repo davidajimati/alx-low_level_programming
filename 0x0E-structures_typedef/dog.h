118872489b7b1bb4c990611aa3875e8c989aaa2e
ajimatidavid <ajimatidavid@gmail.com>
2022-10-12 05:11:32 +0100

    strct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,13 +15,6 @@ struct dog
 	char *owner;
 };
 
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
 
-typedef struct dog_t;
-
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-
-#endif /* DOG_H */
+#endif
