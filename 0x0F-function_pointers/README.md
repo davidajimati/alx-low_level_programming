d1db649559a4d5eb3e8de992b80972f601adc523
ajimatidavid <ajimatidavid@gmail.com>
2022-10-12 19:11:54 +0100

    Header file added

---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+This directory contains projects under pointer to functions
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,10 @@
+#ifndef FUNTION_POINTERS
+#define FUNTION_POINTERS
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+
+#endif /* FUNTION_POINTERS */
