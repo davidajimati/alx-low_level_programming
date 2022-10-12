8c5eee8bf737e85a8ed6ac0e516bf4dae838a231
ajimatidavid <ajimatidavid@gmail.com>
2022-10-12 22:42:44 +0100

    op functions updated

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -13,21 +13,50 @@ int op_add(int a, int b)
 	return (a + b);
 }
 
+
+/**
+ * op_sub - adds a, b
+ *
+ * @a: first integer
+ * @b: second integer
+ * Return: result of the subtraction
+ */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
+/**
+ * op_mul - adds a, b
+ *
+ * @a: first integer
+ * @b: second integer
+ * Return: result of the multiplication
+ */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
+/**
+ * op_div - adds a, b
+ *
+ * @a: first integer
+ * @b: second integer
+ * Return: result of the division
+ */
 int op_div(int a, int b);
 {
 	return (a / b);
 }
 
+/**
+ * op_mod - adds a, b
+ *
+ * @a: first integer
+ * @b: second integer
+ * Return: returns the remainder of a, b division
+ */
 int op_mod(int a, int b)
 {
 	return (a % b);
