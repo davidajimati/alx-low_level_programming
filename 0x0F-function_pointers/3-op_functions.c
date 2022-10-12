052ff82d92a9457ef7cfc2c57026c41e3d1b3629
ajimatidavid <ajimatidavid@gmail.com>
2022-10-12 22:38:20 +0100

    op funtions added

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,34 @@
+#include "3-calc.h"
+#include <stdlib.h>
+
+/**
+ * op_add - adds a, b
+ *
+ * @a: first integer
+ * @b: second integer
+ * Return: result of the addition
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+int op_div(int a, int b);
+{
+	return (a / b);
+}
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
