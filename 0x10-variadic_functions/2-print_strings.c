5d054ff6d3ddeec0cc8eff653549634d08dc35e1
ajimatidavid <ajimatidavid@gmail.com>
2022-10-14 05:35:48 +0100

    nil output edited

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -24,7 +24,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		string = va_arg(ap, char *);
 		
 		if (string == NULL)
-			printf("nil");
+			printf("(nil)");
 		else
 		{
 			printf("%s", string);
