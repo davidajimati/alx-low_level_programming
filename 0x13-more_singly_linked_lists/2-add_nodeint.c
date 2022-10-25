214f1a9b2d50b9d86a10c83c14172a51b0cb5105
ajimatidavid <ajimatidavid@gmail.com>
2022-10-25 23:54:53 +0100

    add_noindent added

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -15,7 +15,6 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	if (head == NULL)
 		return (NULL);
 
-	/* head = &new_node; */
 	new_node->n = n;
 	new_node->next = *head;
 	(*head) = new_node;
