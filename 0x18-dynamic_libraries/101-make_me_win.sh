2584925bbae900f2711e3c5a004f07d85b198c30
davidajimati <ajimatidavid@gmail.com>
2022-12-12 23:14:01 +0100

    updates

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/win.so https://github.com/Zevrov/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/win.so
+https://github.com/holbertonschool/0x18.c/blob/master/101-md5_gm
 export LD_PRELOAD=/tmp/win.so
