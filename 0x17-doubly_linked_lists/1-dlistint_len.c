766030c1504a390a8112ec84e9d1288150e13c21
davidajimati <ajimatidavid@gmail.com>
2022-12-27 23:27:41 +0100

    dlistint length function

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -9,7 +9,7 @@
 
 size_t dlistint_len(const dlistint_t *h)
 {
-	dlistint_t *node = h;
+	dlistint_t const *node = h;
 	size_t count = 0;
 
 	while (node != NULL)
@@ -18,4 +18,30 @@ size_t dlistint_len(const dlistint_t *h)
 		node = node->next;
 	}
 	return (count);
+}
+
+
+int main(void)
+{
+    dlistint_t *head;
+    dlistint_t *new;
+    dlistint_t hello = {8, NULL, NULL};
+    size_t n;
+
+    head = &hello;
+    new = malloc(sizeof(dlistint_t));
+    if (new == NULL)
+    {
+        dprintf(2, "Error: Can't malloc\n");
+        return (EXIT_FAILURE);
+    }
+    new->n = 9;
+    head->prev = new;
+    new->next = head;
+    new->prev = NULL;
+    head = new;
+    n = dlistint_len(head);
+    printf("-> %lu elements\n", n);
+    free(new);
+    return (EXIT_SUCCESS);
 }
