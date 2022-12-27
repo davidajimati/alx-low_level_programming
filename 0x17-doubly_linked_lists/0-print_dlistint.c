5190d2012a5e46375150fec488ad985b369c94f3
davidajimati <ajimatidavid@gmail.com>
2022-12-27 20:18:10 +0100

    print dlistint

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * main -
+*/
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d", h->n);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
