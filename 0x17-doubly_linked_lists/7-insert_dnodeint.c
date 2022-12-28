12af2e8d28249fcfe33de49f0dd6ac6669e52d66
davidajimati <ajimatidavid@gmail.com>
2022-12-28 14:35:34 +0100

    insert node at indent updated

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,36 +1,55 @@
+
+
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - adds node at index
- * @h: head pointer
- * @idx: target index
- * @n: node data
- * Return: Address of new node or NULL if fail
-*/
-
+ * insert_dnodeint_at_index - inserts a node node at a given position
+ * in a dlistint_t list.
+ * @h: pointer to the list.
+ * @idx: position to add the node.
+ * @n: data for the new node.
+ * Return: the address of the new node, or NULL if it failed
+ **/
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *trav = *h;
-	unsigned int count = 0;
-	dlistint_t *new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-		return (NULL);
+	dlistint_t *aux_node = *h, *new_node;
+	unsigned int index, cont = 0;
 
-	if (trav)
+	/* create node */
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+
+	/* border case for insert at the beginning */
+	if (idx == 0)
 	{
-		new->n = n;
-		while (trav != NULL)
-		{
-			if (count == idx)
-			{
-				new->prev = trav->prev;
-				trav->next = trav;
-				trav = new;
-				return (new);
-			}
-			trav = trav->next;
-			count++;
-		}
+		new_node->prev = NULL;
+		new_node->next = *h;
+		if (*h)
+			(*h)->prev = new_node;
+		*h = new_node;
+		return (*h);
 	}
+
+	/* search of position to insert */
+	index = idx - 1;
+	while (aux_node && cont != index)
+	{
+		cont++;
+		aux_node = aux_node->next;
+	}
+
+	/* general case */
+	if (cont == index && aux_node)
+	{
+		new_node->prev = aux_node;
+		new_node->next = aux_node->next;
+		if (aux_node->next)
+			aux_node->next->prev = new_node;
+		aux_node->next = new_node;
+		return (new_node);
+	}
+	free(new_node);
 	return (NULL);
 }
