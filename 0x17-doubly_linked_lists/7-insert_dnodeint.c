8d6ebc0d307e8b9e74fef8e86d008f8b25edcfc2
davidajimati <ajimatidavid@gmail.com>
2022-12-28 12:26:33 +0100

    count incremented

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -29,6 +29,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 				return (new);
 			}
 			trav = trav->next;
+			count++;
 		}
 	}
 	return (NULL);
---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - deletes node at index
+ * @head: head pointer
+ * @index: index of the node
+ * Return: 1 if success, else -1
+*/
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+
+}
