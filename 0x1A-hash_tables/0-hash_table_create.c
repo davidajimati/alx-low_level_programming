49b69d30495012e037b90b7164fe7d5709144b85
davidajimati <ajimatidavid@gmail.com>
2023-01-06 19:45:11 +0100

    size of allocated memory adjusted

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,13 +8,13 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned int i = 0;
-	hash_table_t *ht = malloc(sizeof(hash_table_t) * size);
+	hash_table_t *ht = malloc(sizeof(hash_table_t));
 
 	if (ht == NULL)
 		return (NULL);
 
 	ht->size = size;
-	ht->array = malloc(sizeof(hash_node_t *) * size);
+	ht->array = malloc(sizeof(hash_node_t *));
 
 	if (ht->array ==  NULL)
 		return (NULL);
