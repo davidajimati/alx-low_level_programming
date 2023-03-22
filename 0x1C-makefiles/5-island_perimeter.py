927b92259276f373131476505b6c470f2711750d
davidajimati <ajimatidavid@gmail.com>
2023-03-22 17:46:02 +0100

    100-makefiles created

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# function def island_perimeter(grid): that returns the perimeter of an island described in grid
+
+def island_perimeter(grid):
+    perimeter = 0
+    return perimeter
