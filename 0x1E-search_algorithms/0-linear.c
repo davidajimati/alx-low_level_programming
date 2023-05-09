223806059deaecfa15d4cadbed88f6514fcec4e2
davidajimati <ajimatidavid@gmail.com>
2023-05-09 11:22:28 +0100

    while loop, for-removed

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -18,11 +18,12 @@ int linear_search(int *array, size_t size, int value)
 		return (-1);
 	}
 
-	for (i; i < size; i++)
+	while ( i < size)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
+		i++;
 	}
 
 	return (-1);
